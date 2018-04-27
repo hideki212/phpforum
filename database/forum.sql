47f7a0f71263ebb16a1141d4f133f8627fb9d28c
Yyueei <w.leaf@hotmail.com>
2018-04-27 11:11:00 +0800

    add database

--- database/forum.sql
+++ database/forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 17, 2018 at 09:55 AM
+-- Generation Time: Apr 27, 2018 at 03:10 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -59,19 +59,37 @@ CREATE TABLE IF NOT EXISTS `replies` (
   `Author` varchar(50) NOT NULL,
   `Reply` text NOT NULL,
   `Date_Posted` date NOT NULL,
+  `name` longtext,
+  `type` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`ReplyId`),
   KEY `CategoryId` (`CategoryId`),
   KEY `SubcategoryId` (`SubcategoryId`),
   KEY `TopicId` (`TopicId`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `replies`
 --
 
-INSERT INTO `replies` (`ReplyId`, `CategoryId`, `SubcategoryId`, `TopicId`, `Author`, `Reply`, `Date_Posted`) VALUES
-(1, 1, 2, 4, 'test1', '<br />with your small white dick', '2018-04-16'),
-(2, 1, 2, 4, 'test1', 'with your small white cock', '2018-04-16');
+INSERT INTO `replies` (`ReplyId`, `CategoryId`, `SubcategoryId`, `TopicId`, `Author`, `Reply`, `Date_Posted`, `name`, `type`) VALUES
+(1, 1, 2, 4, 'test1', '<br />with your small white dick', '2018-04-16', '', ''),
+(2, 1, 2, 4, 'test1', 'with your small white cock', '2018-04-16', '', ''),
+(3, 2, 3, 5, 'test1', 'bbb', '2018-04-21', '', ''),
+(4, 1, 2, 1, 'test1', 'pussy~', '2018-04-21', '', ''),
+(5, 2, 3, 7, 'test1', 'dumb', '2018-04-22', '', ''),
+(6, 2, 3, 7, 'test1', 'dumb', '2018-04-22', '', ''),
+(7, 2, 3, 5, 'test1', 'asdasdsa', '2018-04-27', '5ae27dfbbddc78.51800607.mp4', 'video/mp4'),
+(8, 2, 3, 5, 'test1', '', '2018-04-27', '5ae27e0f029aa0.42846871.jpg', 'image/jpeg'),
+(9, 2, 3, 6, 'test1', '', '2018-04-27', '5ae27f477e7224.75717069.jpg', 'image/jpeg'),
+(10, 2, 3, 6, 'test1', 'assddff', '2018-04-27', '5ae27faa8e5617.54823339.jpg', 'image/jpeg'),
+(11, 2, 3, 6, 'test1', 'zzzz', '2018-04-27', '', ''),
+(12, 2, 3, 6, 'test1', 'zzzz', '2018-04-27', '', ''),
+(13, 2, 3, 7, 'test1', '', '2018-04-27', '5ae28a7b69f8c9.79701236.gif', 'image/gif'),
+(14, 2, 3, 5, 'test1', '', '2018-04-27', '', ''),
+(15, 2, 3, 5, 'test1', 'asdas', '2018-04-27', '', ''),
+(16, 2, 3, 5, 'test1', '', '2018-04-27', '5ae28c3c1cdfc6.37374599.gif', 'image/gif'),
+(17, 2, 3, 5, 'test1', '', '2018-04-27', '', ''),
+(18, 2, 3, 7, 'test1', '', '2018-04-27', '', '');
 
 -- --------------------------------------------------------
 
@@ -117,20 +135,29 @@ CREATE TABLE IF NOT EXISTS `topics` (
   `Date_Posted` date NOT NULL,
   `User_Views` int(11) NOT NULL,
   `replies` int(10) UNSIGNED NOT NULL,
+  `name` longtext,
+  `type` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`TopicId`),
   KEY `CategoryId` (`CategoryId`),
   KEY `SubcategoryId` (`SubcategoryId`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `topics`
 --
 
-INSERT INTO `topics` (`TopicId`, `CategoryId`, `SubcategoryId`, `Author`, `Title`, `Content`, `Date_Posted`, `User_Views`, `replies`) VALUES
-(1, 1, 2, 'Joshua', 'Pussy', 'Where to find pussy in malaysia ?', '2017-12-17', 113, 0),
-(2, 1, 2, 'me', 'How to do this', 'fuck your mom Suck A dick you black Fuck', '2018-04-16', 102, 0),
-(3, 1, 2, 'me', 'How to do this', 'fuck your mom Suck A dick you black Fuck', '2018-04-16', 109, 0),
-(4, 1, 2, 'me', 'How to do this', 'fuck your mom Suck A dick you black Fuck', '2018-04-16', 141, 0);
+INSERT INTO `topics` (`TopicId`, `CategoryId`, `SubcategoryId`, `Author`, `Title`, `Content`, `Date_Posted`, `User_Views`, `replies`, `name`, `type`) VALUES
+(1, 1, 2, 'Joshua', 'Pussy', 'Where to find pussy in malaysia ?', '2017-12-17', 118, 0, NULL, NULL),
+(2, 1, 2, 'me', 'How to do this', 'fuck your mom Suck A dick you black Fuck', '2018-04-16', 106, 0, NULL, NULL),
+(3, 1, 2, 'me', 'How to do this', 'fuck your mom Suck A dick you black Fuck', '2018-04-16', 120, 0, NULL, NULL),
+(4, 1, 2, 'me', 'How to do this', 'fuck your mom Suck A dick you black Fuck', '2018-04-16', 165, 0, NULL, NULL),
+(5, 2, 3, 'test1', 'aaa', 'aaa', '2018-04-21', 113, 4, NULL, NULL),
+(6, 2, 3, 'test1', 'asdasd', 'asdas', '2018-04-27', 20, 1, '5ae27f379a7ac1.04626547.mp4', 'video/mp4'),
+(7, 2, 3, 'test1', '', '', '2018-04-27', 12, 2, '5ae28a464e5dc1.17886853.gif', 'image/gif'),
+(8, 2, 3, 'test1', 'sadas', 'sadsasadas', '2018-04-27', 1, 0, '', ''),
+(9, 1, 1, 'test1', 'sadassadas', 'asdas', '2018-04-27', 1, 0, '', ''),
+(10, 1, 1, 'test1', '', '', '2018-04-27', 1, 0, '', ''),
+(11, 1, 1, 'test1', 'sdasdassad', 'sadsa', '2018-04-27', 1, 0, '', '');
 
 -- --------------------------------------------------------
 
